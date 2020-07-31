349c081dc1b642c3e5ddf99dee522772bef0fc7f
deepak <dk721572@gmail.com>
2020-07-31 13:46:51 +0530

    gh-pages deployed 😀

--- src/GameStateContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-const GameStateContext = createContext<
-  [string[], (currentState: string[]) => void]
->([['', '', '', '', '', '', '', '', ''], () => {}]);
-
-export default GameStateContext;
